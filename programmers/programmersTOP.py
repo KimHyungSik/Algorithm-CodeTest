3431b50695217b15e4c3137bf3faf9616ac67784
Kim <gudtlr@gmail.com>
2020-05-10 18:22:26 +0900

    2020.5.10

---
+++ programmers/programmersTOP.py
@@ -0,0 +1,9 @@
+def solution(heights):
+    answer = []
+    for i in range(len(heights)):
+        answer.append(0)
+        for j in range(i-1,-1,-1):
+            if heights[j]>heights[i]:
+                answer[i] = j+1
+                break
+    return answer
