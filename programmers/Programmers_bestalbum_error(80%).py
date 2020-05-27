6593c7a7b97a4c94ecf51ae54856b1c08a3ca9f9
KimHyungSik <54847106+KimHyungSik@users.noreply.github.com>
2020-05-27 20:00:00 +0900

    fxxk error

---
+++ programmers/Programmers_bestalbum_error(80%).py
@@ -0,0 +1,21 @@
+def solution(genres, plays):
+    answer = []
+    onecchk=[0 for i in range(len(genres))]
+    Dgenres = {}
+    for g,p in zip(genres, plays):
+        if g in Dgenres:
+            Dgenres[g].append(p)
+        else:
+            Dgenres[g] = [p]
+    for g in genres:
+        Dgenres[g] = sorted(Dgenres[g], reverse=True)
+        Dgenres[g] = Dgenres[g][:2] 
+    Dgenres = sorted(Dgenres.items(), reverse=True)
+    
+    for d in Dgenres:
+        for nd in d[1]:
+            for a in range(len(genres)):
+                if d[0] == genres[a] and nd == plays[a] and onecchk[a] == 0:
+                  answer.append(a)
+                  onecchk[a] = 1                  
+    return answer
