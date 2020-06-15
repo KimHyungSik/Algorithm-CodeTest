e357ae644349133b72266606e5076ea75bf8826b
KimHyungSik <54847106+KimHyungSik@users.noreply.github.com>
2020-06-15 09:11:10 +0900

    Update programmers_bigsu.py

--- programmers/programmers_bigsu.py
+++ programmers/programmers_bigsu.py
@@ -2,11 +2,17 @@ def solution(numbers):
   answer = ''
   arr = sorted(numbers, key = lambda x : (str(x)[0],x),reverse=True)
   for i in range(len(arr)-1):
-    if str(arr[i])[0] == str(arr[i+1])[0] and arr[i]%10 < arr[i+1]%10:
-      temp = arr[i]
-      arr[i] = arr[i+1]
-      arr[i+1] = temp
+    if str(arr[i])[0] == str(arr[i+1])[0]:
+      if int(str(arr[i])+str(arr[i+1]))<int(str(arr[i+1])+str(arr[i])):
+        temp = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1] = temp
   answer = "".join(map(str, arr))
   answer = int(answer)
   answer = str(answer)
   return answer
+#==================================
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
